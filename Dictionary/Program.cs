9de0f3178c11bc1ba1fac9795081eaa6a138a0ef
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-07 07:39:07 -0600

    Learned Dictionary

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Dictionary<string, string> lookUp = new Dictionary<string, string>();
+
+lookUp["Fish"] = "Can swim";
+lookUp["Tiger"] = "Only eat meat";
+lookUp["Dog"] = "Human best buddy";
+
+Console.WriteLine($"Why is people like dogs? Because dogs are {lookUp["Dog"]}");
